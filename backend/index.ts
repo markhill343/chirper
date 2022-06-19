556391c3a9053f048bd402c47f85c71651c21a49
markhill <markus.hillreiner@hs-augsburg.de>
2022-06-19 15:53:14 +0200

    added follwing packages: -axios ( to use promised based http) -core.js -sass-loader (for loading sass and compiling into css) -vuex (state managment for vue.js)
    -added index.ts with basic constants and connection to the database

---
+++ backend/index.ts
@@ -0,0 +1,26 @@
+// Set constants for the application
+const app = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+//to do create the models
+const user =
+const message =
+
+const port = process.env.PORT || 3000
+const MongoDB_URI = process.env.MONGODB_URI || 'mongodb://localhost/chriper'
+
+// Connecting to the database
+mongoose.connect(MongoDB_URI, { 
+    useNewUrlParser: true 
+},() => {
+    console.log('Connected to ChirperDB')
+});
+
+// Define cors and body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+//Backend starts here
