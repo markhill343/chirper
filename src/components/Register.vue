18a24534971946eee343ea3facfead4b0e410242
markhill <markus.hillreiner@hs-augsburg.de>
2022-07-05 16:38:36 +0200

    added register.vue

---
+++ src/components/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <ul>
+      <li><input type="text" id="Name" placeholder="Name" /></li>
+      <li><input type="username" id="Username" placeholder="Username" /></li>
+      <li><input type="email" id="Email" placeholder="Email" /></li>
+      <li><input type="password" id="Password" placeholder="Password" /></li>
+    </ul>
+
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
