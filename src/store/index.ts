944123ddeeb2df319cd263d71f7d0bcd3da9891f
markhill <markus.hillreiner@hs-augsburg.de>
2022-07-09 23:46:57 +0200

    updated store to a custom one

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,14 +5,48 @@ export const store = createStore({
   state: {
     isLoading: true,
 
+    loginUsername: "",
+    loginPasswordModel: "",
+    loginErrors: null,
+
     registerUsernameModel: "",
     registerEmailModel: "",
     registerPasswordModel: "",
     registerNameModel: "",
     registerErrors: "",
 
-    loginUsernameOrEmailModel: "",
-    loginPasswordModel: "",
-    loginErrors: null,
+    newInfos: {
+      name: "",
+      mail: "",
+      website: "",
+      bio: "",
+      location: "",
+      profile_image: "",
+      bannerImage: "",
+    },
+
+    newTweet: {
+      text: "",
+      image: "",
+    },
+
+    registerPage: false,
+    addTweetPage: false,
+    editProfilePopup: false,
+    userId: localStorage.getItem("userId"),
+
+    repliedTweet: {},
+
+    zoomedImage: "",
+
+    currentUser: {},
+
+    userForProfile: {},
+
+    tweetForDetail: {},
+
+    bookmarks: {},
+
+    tweets: [],
   },
 });
