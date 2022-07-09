20dff61d5277fa914ae4e7f0917b3a1a42f42a8b
markhill <markus.hillreiner@hs-augsburg.de>
2022-07-09 23:50:51 +0200

    -added some form components

---
+++ src/components/form/ChirperButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="chirper-button-root">
+    <button
+      @click="chirperHandler()"
+      :class="[isBlueBg ? 'blueBg' : 'whiteBg']"
+      :style="{ height: h, width: w, borderRadius: br }"
+      v-html="text"
+    ></button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["clickEvent"],
+  methods: {
+    chirperHandler() {
+      try {
+        this[this.clickEvent]();
+      } catch (e) {
+        console.log(
+          `Error in ChirperButton components Chirper handler method:\n ${e}`
+        );
+      }
+    },
+  },
+};
+</script>
---
+++ src/components/form/PressButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <span>
+    <i @click="handleAction()" class="actionButton"></i>
+  </span>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["action"],
+  methods: {
+    handleAction() {
+      try {
+        this[this.action]("Button pressed");
+      } catch (e) {
+        console.log(
+          `Error in ActionButton components 'handleAction' method:\n ${e}`
+        );
+      }
+    },
+  },
+};
+</script>
